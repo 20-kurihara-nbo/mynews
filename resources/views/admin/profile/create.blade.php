864b9f0ab7e89cebe3131d782d368f8f3a717824
kurihara-nbo <kurihara.nbo@gmail.com>
2019-08-04 04:36:48 +0000

    add views/adomin/profile
    laravel 09 課題提出　add Action, edit Action bladeファイル作成

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
+     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MyNews</title>
+        <title>profilecreate</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        <h1>プロフィール作成画面</h1>
     </body>
 </html>
