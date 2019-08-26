efa3845c3c38039b19db17192bdcc0779ecfb0fa
kurihara-nbo <kurihara.nbo@gmail.com>
2019-08-26 04:56:52 +0000

    update NewsController.php
    createにアクセスするとadmin/news/createにリダイレクトする

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,5 +12,10 @@ public function add()
     {
         return view('admin.news.create');
     }
+public function create(Request $request)
+    {
+    // admin/news/createにリダイレクトする
+    return redirect('admin/news/create');
+    }
 }
 
