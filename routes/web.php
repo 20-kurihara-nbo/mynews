8824d031447574924ef14322be6266966dfb9b6a
kurihara-nbo <kurihara.nbo@gmail.com>
2019-08-26 04:45:13 +0000

    update web.php
    admin/profile/editにログインせずにアクセスするとログイン画面にリダイレクトされるように設定しました

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('XXX','XXX\AAAController@BBB');
 Route::group(['prefix' => 'admin'],function()
 {
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 
 Auth::routes();
 
