cadae64b841cc0758dbba35b00d4ca50f04f7c1b
kurihara-nbo <kurihara.nbo@gmail.com>
2019-08-26 05:07:38 +0000

    update web.php
    routingを編集して、news/createにpostリクエストでアクセスするとAdmin\NewsControllerのアクションcreateに渡すようにしました。

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'],function()
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'],function()
 {
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
 });
 
 //laravel09 課題１　routing
