6971503b6baa05f19779ec07eed1605aa83e75e1
kurihara-nbo <kurihara.nbo@gmail.com>
2019-08-30 16:18:12 +0000

    update routes/web.php
    web.phpを編集して、admin/profile/editにpostメソッドでアクセスしたら、ProfileController の update Action に割り当てるように設定しました。

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::group(['prefix' => 'admin'],function()
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
     Route::post('profile/create','Admin\ProfileController@create');
+    Route::post('profile/edit','Admin\ProfileController@update Action');
 
 Auth::routes();
 
