22fef93c1d6c97b24be97fdd0785931fdc4949f9
kurihara-nbo <kurihara.nbo@gmail.com>
2019-10-06 15:12:30 +0000

    update app/Http/Controllers/Admin/NewsController.php
    NewsController.phpにindex Actionを追加。

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -42,5 +42,23 @@ class NewsController extends Controller
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
+    
+    //アクセス制限なしのユーザ定義関数index
+    public function index(Request $request)
+    {
+        //$requestの中のcond_titleの値を代入。$requestに値がなければnull
+        $cond_title = $request->cond_title;
+        //もし$cond_titleがnull出ないなら
+        if ($cond_title !=''){
+            //検索されたら検索結果を取得して代入。入力した文字に一致するレコードを全て取得。
+            $posts=News::where('title', $cond_title)->get();
+        } else {
+            //それ以外は全てニュースを取得する
+            $posts=News::all();
+        }
+        //Requestにcond_titleを送っている
+        return view('admin.news.index', ['posts' => $posts,'cond_title'=>$cond_title]);
+        
+    }
 }
     
