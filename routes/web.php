a989d6b9b69fd3cbb6b9eec63b4ae48d1d7f9697
kurihara-nbo <kurihara.nbo@gmail.com>
2019-10-10 02:33:27 +0000

    update routes/web.php
    web.phpに更新と削除のルーティングを実装しました

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'],function() {
+Route::group(['prefix' => 'admin'],function() {
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
     Route::post('news/create','Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
 });
 
 //laravel09 課題１　routing
