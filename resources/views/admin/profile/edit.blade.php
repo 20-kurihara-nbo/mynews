0f9f61b987d086b5e956e036db595a8b6f6d38da
kurihara-nbo <kurihara.nbo@gmail.com>
2019-10-14 14:19:04 +0000

    update profile/edit.blade.php
    プロフィール編集用のページを作成しました。

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -20,7 +20,7 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="name">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
 
@@ -45,13 +45,10 @@
                             <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <div class="col-md-10">
-                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
-                            {{ csrf_field() }}
-                            <input type="submit" class="btn btn-primary" value="更新">
-                        </div>
-                    </div>
+                     
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                    
                 </form>
             </div>
         </div>
