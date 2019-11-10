247802dfe73f23261ea3432212c650f79271fea3
kurihara-nbo <kurihara.nbo@gmail.com>
2019-11-10 14:44:50 +0000

    update Profile.php
    Profile.phpを編集してprofileeditとの関連を定義しました

--- app/Profile.php
+++ app/Profile.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = array('id');//
+    
     public static $rules = array(
         'name' => 'required',
         'hobby' => 'string',
         'introduction' => 'string',
     );
+    
+    //profileeditモデルに関連付けを行う
+    public function profileedit()
+    {
+        return $this->hasmany('app\profileedit');
+    }
 }
