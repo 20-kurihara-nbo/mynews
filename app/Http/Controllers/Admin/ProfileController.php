12abf6d1b5a296feedb79a429b565803269920ee
kurihara-nbo <kurihara.nbo@gmail.com>
2019-11-14 02:15:23 +0000

    update ProfileController.php
    プロフィールコントローラーのアップデートアクションでプロフィールモデルを保存するタイミングでプロフィールエディットモデルにも編集履歴を追加するように実装しました。

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\Controller;
 //以下によりProfile Modelを扱えるように設定
 use App\Profile;
 
+//profileedit modelの使用を宣言
+use App\profileedit;
+
+//carbonの使用を宣言
+use carbon\carbon;
+
 class ProfileController extends Controller
 {
     //laravel08 コントローラーとルーティング　課題５
@@ -49,7 +55,14 @@ class ProfileController extends Controller
 
       // 該当するデータを上書きして保存する
       $profile->fill($profile_form)->save();
-    
+      
+      //該当するデータをprofileeditモデルへ送信
+      $profileedit=new profileedit;
+      $profileedit->profile_id = $Profile->id;
+      $profileedit->edited_at = Carbon::now();
+      $profileedit->save();
+      
+      
         return redirect('admin/profile/edit');
     }
 }
