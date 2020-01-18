7eebcb0bd11baac2a6824de76637cdaba0157530
kurihara-nbo <kurihara.nbo@gmail.com>
2020-01-18 16:01:51 +0000

    add profile/index.blade.php
    profile/index.blade.phpを編集して描画するように実装しました。

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,59 +1,69 @@
-@extends('layouts.front')
+@extends('layouts.profile')
+
+@section('title','プロフィール情報')
 
 @section('content')
     <div class="container">
-        <hr color="#c0c0c0">
-        @if (!is_null($headline))
-            <div class="row">
-                <div class="headline col-md-10 mx-auto">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/'.$headline->image_path) }}">
-                                    @endif    
-                                </div>
-                                <div class="title p-2">
-                                    <h1>{{ str_limit($headline->name, 70) }}</h1>
-                                </div>
-                            </div>
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール情報</h2>
+                <form action="{{ action('ProfileController@index') }}" method="post" enctype="multipart/form-date">
+                    
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach    
+                        </ul>
+                    @endif
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
                         </div>
-                        <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->hobby, 650) }}</p>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10 form-inline">
+                            <input type="radio" class="form-control" name="gender" value="man" checked>男性
+                            <input type="radio" class="form-control" name="gender" value="woman">女性
                         </div>
+                    </div>
+
+　　　　　　　　　　<div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                     
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                    
+                </form> 
+                {{-- 以下を追記　--}}
+                <div class="row mt-5">
+                    <div class="col-md-4 mx-auto">
+                        <h2>編集履歴</h2>
+                        <ul class="list-group">
+                            @if ($profile_form->profileedit != NULL)
+                                @foreach ($profile_form->profileedit as $profileedit)
+                                    <li class="list-group-item">{{ $profileedit->edited_at }}</li>
+                                @endforeach
+                            @endif
+                        </ul>
                     </div>
                 </div>
             </div>
-        @endif
-        <hr color="#c0c0c0">
-        <div class="row">
-            <div class="posts col-md-8 mx-auto mt-3">
-                @foreach($posts as $post)
-                    <div class="post">
-                        <div class="row">
-                            <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
-                                <div class="name">
-                                    {{ str_limit($post->name, 150) }}
-                                </div>
-                                <div class="hobby mt-3">
-                                    {{ str_limit($post->hobby, 1500) }}
-                                </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/'.$post->image_path) }}">
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <hr color="#c0c0c0">
-                @endforeach
-            </div>
         </div>
-    </div>
     </div>
 @endsection
